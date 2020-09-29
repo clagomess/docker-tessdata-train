675c54643aca2956337a664e19b808db0a6d04a8
Claudio Gomes <cla.gomess@gmail.com>
2020-09-29 02:29:30 -0300

    nproc

--- crunch.sh
+++ crunch.sh
@@ -4,7 +4,7 @@ crunch 3 3 ABCDEFGHIJKLMNOPQRSTUVWXYZ | while read bloco; do
   cd /srv/tesstrain/data/plc-ground-truth && crunch 8 8 -d 3,% -t "${bloco}-%,%%" | php gerar-placa.php
 #  cd /srv/tesstrain/data/plc-ground-truth && crunch 8 8 -d 3,% -t "${bloco}-1234" | php gerar-placa.php # teste
 
-  cd /srv/tesstrain && make training MODEL_NAME=plc PSM=7
+  cd /srv/tesstrain && make training MODEL_NAME=plc PSM=7 CORES=`nproc`
 
   cd /srv/tesstrain/data/plc-ground-truth/ \
   && rm -rf *.lstmf \
